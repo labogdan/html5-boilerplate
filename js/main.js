2679808222670fc3a11563bbdba1c26d1439fea2
unknown <VMAdmin@.(none)>
2014-02-11 16:50:23 -0700

    added weather widget
    Basic functionality - hardcoded

--- js/main.js
+++ js/main.js
@@ -1,71 +1,62 @@
-// The module pattern is a good way to encapsulate your JavaScript functions
-  // and properties.  The example below shows how to create a module, namespace
-  // it, and specify public/private properties and functions.
-
-  // Be aware that the Module Pattern is NOT generally used for object-oriented
-  // programming.  It does not give you the ability to define a class that can
-  // spawn objects based on its definition.  Although it is possible to jerry
-  // rig the Module Pattern to define classes, there isn't a real benefit to
-  // doing this as JavaScript has another facility for this.
-
-  // If you are more interested in laying out a classical object-oriented
-  // hierarchy, look at this example instead:
-  // https://github.com/stevekwan/experiments/blob/master/javascript/class.html
-
-  // However, be aware that JavaScript is not a classically object-oriented
-  // language.  If you are trying to write true classical OOP in JavaScript,
-  // you're probably doing it wrong.  :)
-
-  // Advantages of the Module Pattern:
-  // -Avoids nonsense with the "this" keyword, which often causes confusion
-  // -Gives you basic encapsulation without having to write a lot of code
-  // -If you are creating a singleton "class," the Module Pattern is for you.
-
-  // Disadvantages of the Module Pattern:
-  // -True class-based OOP doesn't work well with the Module Pattern.
-
-  // First, let's create a namespace.  It's not required, but it's usually a
-  // good idea.
+  $(document).ready(function(){
+  
   var MyNamespace = MyNamespace || {};
 
   MyNamespace.MyModule = function()  {
 
-    var myPrivateProperty = 2;
     var myPublicProperty = 1;
-
-    var myPrivateFunction = function()  {
-      console.log("myPrivateFunction()");
-    };
-
-    var myPublicFunction = function()   {
-      console.log("myPublicFunction()");
-
-      myPrivateFunction();
-    };
+    
+    var WEATHERURL = "http://api.wunderground.com/api/c7843c7302d3a7db/geolookup/conditions/q/";
+    var URLRESPONSE = ".json";
 
     var init = function()   {
       // Do some setup stuff
       console.log("init()");
     };
 
-    // This is the part that separates the private and public stuff.  Anything
-    // in this object becomes public.  Anything NOT in this object becomes
-    // private.
+    var myPrivateFunction = function()  {
+    	console.log("myPrivateFunction()");
+    };
+
+    var myPublicFunction = function()   {
+    	console.log("myPublicFunction()");
+	    myPrivateFunction();
+    };
+
+	var queryLocation = function(city, state) {
+		console.log('finding weather for '+city+state);
+
+		$.ajax({
+			url : WEATHERURL + "VA/Blacksburg" + URLRESPONSE,
+			dataType : "jsonp",
+			success : function(parsed_json) {
+				var location = parsed_json['location']['city'];
+				var temp_f = parsed_json['current_observation']['temp_f'];
+				alert("Current temperature in " + location + " is: " + temp_f);
+			}
+		});
+	};
+
     var oPublic =
     {
       init: init,
       myPublicProperty: myPublicProperty,
-      myPublicFunction: myPublicFunction
+      myPublicFunction: myPublicFunction,
+      queryLocation : queryLocation
     };
 
     return oPublic;
   }();
 
-
-	console.log("Calling init()...");
 	MyNamespace.MyModule.init();
 
 	console.log("Calling myPublicFunction()...");
 	MyNamespace.MyModule.myPublicFunction();
 
+	MyNamespace.MyModule.queryLocation();
+
+});
+
   
+
+
