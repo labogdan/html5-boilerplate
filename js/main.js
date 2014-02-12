57da920edb1e101583a5a85fa9ea2f55d11a5707
unknown <VMAdmin@.(none)>
2014-02-11 17:58:08 -0700

    adding method and ui styles

--- js/main.js
+++ js/main.js
@@ -2,21 +2,38 @@
   
   var MyNamespace = MyNamespace || {};
 
-  MyNamespace.MyModule = function()  {
+  MyNamespace.WUnderground = function()  {
 
-    var myPublicProperty = 1;
-    
+	//	Constant Declarations 
+	
     var WEATHERURL = "http://api.wunderground.com/api/c7843c7302d3a7db/geolookup/conditions/q/";
     var URLRESPONSE = ".json";
 
+	//	Private Vars
+
+	//	Public Vars
+
+    var myPublicProperty = 1;
+
+	// Public Methods
+
     var init = function()   {
-      // Do some setup stuff
-      console.log("init()");
+     	// Do some setup stuff
+    	console.log("init()");
     };
 
     var myPrivateFunction = function()  {
     	console.log("myPrivateFunction()");
     };
+
+	// Private Methods
+
+	var updateWidget = function(city, state, temp) {
+		console.log("Current temperature in " + city + " is: " + temp);
+		
+		$('#weatherTitle').html(city + ', ' + state);
+		
+	};
 
     var myPublicFunction = function()   {
     	console.log("myPublicFunction()");
@@ -27,12 +44,14 @@
 		console.log('finding weather for '+city+state);
 
 		$.ajax({
-			url : WEATHERURL + "VA/Blacksburg" + URLRESPONSE,
+			url : WEATHERURL + state + "/" + city + URLRESPONSE,
 			dataType : "jsonp",
 			success : function(parsed_json) {
-				var location = parsed_json['location']['city'];
+				console.log(parsed_json);
+				var locationCity = parsed_json['location']['city'];
+				var locationState = parsed_json['location']['state'];
 				var temp_f = parsed_json['current_observation']['temp_f'];
-				alert("Current temperature in " + location + " is: " + temp_f);
+				updateWidget(locationCity, locationState, temp_f);
 			}
 		});
 	};
@@ -48,12 +67,12 @@
     return oPublic;
   }();
 
-	MyNamespace.MyModule.init();
+	MyNamespace.WUnderground.init();
 
 	console.log("Calling myPublicFunction()...");
-	MyNamespace.MyModule.myPublicFunction();
+	MyNamespace.WUnderground.myPublicFunction();
 
-	MyNamespace.MyModule.queryLocation();
+	MyNamespace.WUnderground.queryLocation('blacksburg','va');
 
 });
 
