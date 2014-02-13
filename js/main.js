f14bb7bac0fa0c7c3b78f55a25f88749850180c1
labogdan <abogdan1234@yahoo.com>
2014-02-12 17:35:00 -0700

    lots of changes

--- js/main.js
+++ js/main.js
@@ -1,82 +1,14 @@
-  $(document).ready(function(){
-  
-  var MyNamespace = MyNamespace || {};
+var ManyAPIs = ManyAPIs || {};
 
-  MyNamespace.WUnderground = function()  {
+$(document).ready(function(){
 
-	//	Constant Declarations 
+	ManyAPIs.WUnderground.init();
+
+	ManyAPIs.WUnderground.queryLocation('blacksburg','va');
 	
-    var WEATHERURL = "http://api.wunderground.com/api/c7843c7302d3a7db/geolookup/conditions/q/";
-    var URLRESPONSE = ".json";
-
-	//	Private Vars
-
-	//	Public Vars
-
-    var myPublicProperty = 1;
-
-	// Public Methods
-
-    var init = function()   {
-     	// Do some setup stuff
-    	console.log("init()");
-    };
-
-    var myPrivateFunction = function()  {
-    	console.log("myPrivateFunction()");
-    };
-
-	// Private Methods
-
-	var updateWidget = function(city, state, temp) {
-		console.log("Current temperature in " + city + " is: " + temp);
-		
-		$('#weatherTitleCS').html(city + ', ' + state);
-		$('#weatherTitleTemp').html(temp);
-		
-	};
-
-    var myPublicFunction = function()   {
-    	console.log("myPublicFunction()");
-	    myPrivateFunction();
-    };
-
-	var queryLocation = function(city, state) {
-		console.log('finding weather for '+city+state);
-
-		$.ajax({
-			url : WEATHERURL + state + "/" + city + URLRESPONSE,
-			dataType : "jsonp",
-			success : function(parsed_json) {
-				console.log(parsed_json);
-				var locationCity = parsed_json['location']['city'];
-				var locationState = parsed_json['location']['state'];
-				var temp_f = parsed_json['current_observation']['temp_f'];
-				updateWidget(locationCity, locationState, temp_f);
-			}
-		});
-	};
-
-    var oPublic =
-    {
-      init: init,
-      myPublicProperty: myPublicProperty,
-      myPublicFunction: myPublicFunction,
-      queryLocation : queryLocation
-    };
-
-    return oPublic;
-  }();
-
-	MyNamespace.WUnderground.init();
-
-	console.log("Calling myPublicFunction()...");
-	MyNamespace.WUnderground.myPublicFunction();
-
-	MyNamespace.WUnderground.queryLocation('blacksburg','va');
+	ManyAPIs.WUnderground.queryWithTimer('los angeles', 'ca', 60000);
+	
+	ManyAPIs.WUnderground.stopQuery('los angeles', 'ca');
+  
 
 });
-
-  
-
-
---
+++ js/weather.js
@@ -0,0 +1,75 @@
+ManyAPIs.WUnderground = function()  {
+
+	//	Constant Declarations 
+	var WEATHERURL = "http://api.wunderground.com/api/c7843c7302d3a7db/geolookup/conditions/q/";
+	var URLRESPONSE = ".json";
+
+	//	Private Vars
+	var queryMap = [];
+
+	//	Public Vars
+    var myPublicProperty = 1;
+
+	// Private Methods
+	var updateWidget = function(city, state, temp) {
+		console.log("Current temperature in " + city + " is: " + temp);
+		
+		$('#weatherTitleCS').html(city + ', ' + state);
+		$('#weatherTitleTemp').html(temp);
+	};
+
+	var removeIntervalForCity = function(city,state) {
+		
+		var id = queryMap[city+state] || -1;
+		console.log('id is ' + id + ' for ' + city+state);
+		
+		if (id != -1) { 
+			console.log('clearing interval');
+			clearInterval(id);
+			return true;
+		}
+		return false;
+	};
+
+	// Public Methods
+    var init = function()   {
+     	// Do some setup stuff
+    	console.log("init()");
+    	$( ".draggable" ).draggable({ handle:"div:first-of-type"});
+    };
+
+	var queryLocation = function(city, state) {
+		console.log('finding weather for '+city+state);
+
+		$.ajax({
+			url : WEATHERURL + state + "/" + city + URLRESPONSE,
+			dataType : "jsonp",
+			success : function(parsed_json) {
+				console.log(parsed_json);
+				var locationCity = parsed_json['location']['city'];
+				var locationState = parsed_json['location']['state'];
+				var temp_f = parsed_json['current_observation']['temp_f'];
+				updateWidget(locationCity, locationState, temp_f);
+			}
+		});
+	};
+
+	var queryWithTimer = function(city, state, timer) {
+		console.log('setting ' + city + state + ' for ' + timer);
+		queryMap[city+state] =  setInterval(function(){queryLocation(city, state);}, timer);
+	};
+	
+	var stopQuery = function(city, state) {
+		console.log(removeIntervalForCity(city, state));
+	};
+
+	var oPublic = {
+		init: init,
+		myPublicProperty: myPublicProperty,
+		queryLocation : queryLocation,
+		queryWithTimer : queryWithTimer,
+		stopQuery : stopQuery
+    };
+
+    return oPublic;
+}();
