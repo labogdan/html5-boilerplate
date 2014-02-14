89f7b57382cdcc21b93dfe653520af12f0f5d245
labogdan <abogdan1234@yahoo.com>
2014-02-14 12:26:43 -0700

    ignoring log file + also adding php api to log contents of post from weather page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 var ManyAPIs = ManyAPIs || {};
 
+// remember that html5 workspace needs to be moved to C:\xampp\htdocs\html5
+
 $(document).ready(function(){
 
 	ManyAPIs.WeatherWidget.init();
 
+	$.ajax ({
+		url : 'http://localhost/html5/logResponse.php',
+		type: 'POST',
+		data : {name:'asdf'},
+		success : function(retval) {
+			console.log(retval);
+		}
+	});
 });
---
+++ logResponse.php
@@ -0,0 +1,23 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php 
+ 	echo '<p>Hello World</p>';
+	echo htmlspecialchars($_POST['name']);
+	
+	$file = "log-a-b.txt";
+		
+	if (file_exists($file)) {
+		$current = file_get_contents($file);
+		$current .= $_POST['name'];
+		file_put_contents($file, $current);
+	} else {
+		file_put_contents($file, $_POST['name']);
+	}
+	
+	
+ ?> 
+ </body>
+</html>
